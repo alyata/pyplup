ba22eee35a46b9a30b10d12b15fb5179dddf601d
ar2519 <ar2519@ic.ac.uk>
2020-06-30 17:38:14 +0100

    refactor parser_gen.py so it can be used by parser.py

--- scripts/makeparser.py
+++ scripts/makeparser.py
@@ -6,50 +6,41 @@ message formats
 def makeparser(format):
     ws = format.split('|')
     ws = ws[1:]
+    #remove dashes, as python symbols can't have dashes
+    fname = f'generated_{ws[0].replace("-", "_")}'
+    #construct the function
     s = f"#{format}\n"
-    s += f"def {ws[0]}(tokens: [str]) -> dict:\n"
+    s += f"def {fname}(tokens: [str]) -> dict:\n"
     s+= "    return {\n"
     s += f"        \"TYPE\" : \"{ws[0]}\",\n"
     for i, w in enumerate(ws[1:]):
         s += f"        \"{w}\" : tokens[{i+1}],\n"
     s += "    }\n\n"
-    return (ws[0], s)
+    return (ws[0], fname, s)
 
 if __name__ == "__main__":
     inp = open("message_formats.txt", "r").read()
     out = open("parser_gen.py", "w")
-    out.write('''import json
-
-"""
-A collection of parsing functions to parse Server-to-Client messages.
-As a battle bot, user messages can be ignored. Of particular interest
-are messages pertaining to starting battles and logins.
-"""
-
-def parse(roomid: str, message: str) -> dict:
-    tokens = message.split('|')
-    #remove empty first token
-    tokens = tokens[1:]
-    type = tokens[0]
+    out.write('''def parse_gen(tokens: [str]) -> dict:
     parse_func = {
 ''')
     lines = inp.split("\n")
     funcs = []
     for line in lines:
         if line and line[0] != '#':
-            type, func = makeparser(line)
-            out.write(f'        \"{type}\" : {type},\n')
+            type, fname, func = makeparser(line)
+            out.write(f'        \"{type}\" : {fname},\n')
             funcs.append(func)
     out.write('''
-    }.get(type, unrecognized)
+    }.get(tokens[0], unrecognized)
     parsed = parse_func(tokens)
-    parsed["ROOMID"] = roomid
     return parsed
 
 def unrecognized(tokens: [str]) -> dict:
     return {
         "TYPE" : "unrecognized",
     }
+    
 ''')
     for func in funcs:
         out.write(func)
